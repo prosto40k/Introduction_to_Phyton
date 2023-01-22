8b6d1edbcc88dd64e7345824809105f68e1daa1a
bloody <novickiy.n.l@gmail.com>
2023-01-22 13:24:33 +0300

    first commit

---
+++ Example02(input).py
@@ -0,0 +1,22 @@
+#Куда можно выводить
+#GUI
+#WEB
+#Мобильное приложение
+#Хранилище данных
+#Консоль(Терминал)
+
+print('Hello')
+print(100,99,99-1)
+name='Кеша'
+age =2
+print(name,age)
+print(name,age,sep=',')
+#Перенос строки
+print('/////////////')
+print(name,age,sep=',',end=';')
+#Ввод данных
+print()
+result=input()
+name=input('Как тебя зовут')
+print ('пользователь ',result)
+
