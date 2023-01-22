6014f0937b139b3951c6c4dfd0e7c49baec25eb4
bloody <novickiy.n.l@gmail.com>
2023-01-22 18:09:58 +0300

    Цикл

---
+++ Example05(while).py
@@ -0,0 +1,12 @@
+#цикл while
+name=input('Кто создатель Phyton:')
+while name!= 'Гвидо':
+    print('Неверно')
+    name = input('Кто создатель Phyton:')
+print('Вертно')
+
+number=0
+while number<=100:
+    print(number)
+    # number=number+1
+    number+=1
